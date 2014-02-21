64564b61a3de6141ff0f4b70ad07a02392200f9f
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-02-20 23:32:44 -0500

    First commit

---
+++ 3ex.rb
@@ -0,0 +1,33 @@
+# this writes what's in quotes
+puts "I will now count my chickens:"
+
+# writes what's in quotes, completes operation
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+#writes what's in quotes
+puts "Now I will count the eggs:"
+
+#completes operation, uses order of operations
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#writes quotes
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#completes operation
+puts 3 + 2 < 5 - 7
+
+#writes quotes, completes operation
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+#writes quotes
+puts "Oh, that's why it's false."
+
+#writes quotes
+puts "How about some more."
+
+#writes quotes, completes operation
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
